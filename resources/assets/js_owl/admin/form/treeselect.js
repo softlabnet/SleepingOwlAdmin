9d9c4ac9f4e0f9cdbfb4a7e784e8d74b24562053
vagrant <vagrant@homestead>
2020-05-04 15:41:17 +0000

    123

--- resources/assets/js_owl/admin/form/treeselect.js
+++ resources/assets/js_owl/admin/form/treeselect.js
@@ -1,4 +1,4 @@
-import { TreeSelect } from 'ant-design-vue';
+import { TreeSelect, Spin } from 'ant-design-vue';
 import 'ant-design-vue/dist/antd.css';
 import { relativeTimeThreshold } from 'moment';
 
@@ -27,6 +27,14 @@ Vue.component('element-treeselect', {
             required: false,
             default: ''
         },
+        parent_filter_id:{
+            required: false,
+            default: null
+        },
+        attribute_id:{
+            required: false,
+            default: null
+        },
         url: {
             type: String,
             required: false,
@@ -38,63 +46,79 @@ Vue.component('element-treeselect', {
             list: null,
             val: null,
             formFieldName: 'tree_select_hidden_' + this.name.replace('[]','_'),
-            selected: null
+            selected: null,
+            disabled: false,
+            loading:true,
         }
     },
     template: '<div> \
+        <a-spin :spinning="loading"> \
         <a-tree-select \
+            :disabled="disabled" \
             allowClear \
             showSearch \
             style="width: 100%" \
             :treeData="list" \
+            :loading="loading" \
             v-model="val" \
             :treeCheckable="multiple" \
             :multiple="multiple" \
             treeNodeFilterProp="title" \
-            searchPlaceholder="Please select" \
+            searchPlaceholder="Выберите значение" \
             @select="onSelect" \
             @change="onChange" \
             :dropdownStyle="{ maxHeight: \'400px\', overflow: \'auto\' }" \
         /> \
+        </a-spin> \
         <div :id="formFieldName"> \
         </div> \
         </div>',
     mounted () {
         const self = this;
-        self.list = self.options;
+        self.list = [];//self.options;
         self.val = self.value;
         this.createHiddenFields(self.value);
+        this.loadData(this.url, this.parent_filter_id);
         this.$root.$on('parent_change', (event) => {
             if(event.parent != this.name && event.parent == this.parent && this.parent){
                 let parent_filter_id = event.value;
                 const self = this;
                 self.val = null;
                 this.createHiddenFields(self.val);
-                $.ajax({
-                    url: self.url,
-                    data:{
-                        parent_filter_id: parent_filter_id
-                    },  
-                    method: 'POST',
-                    dataType: 'JSON',
-                    success: function(data) {
-                        self.list = [];
-                        $.each(data.output, function(index,item){
-                            self.list.push({key: item.id, label: item.name, value: item.id});
-                        })
-                        // this.val = '';                        
-                    },
-                    error: function(data) {
-                        console.log(data);
-                    },
-                });
+                this.loadData(self.url, parent_filter_id);
             }
         });
     },
     components: {
-        TreeSelect
+        TreeSelect, Spin
     },
     methods: {
+        loadData(url, parent_filter_id = null){
+            //this.disabled = true;
+            this.loading = true;
+            let self = this;
+            $.ajax({
+                url: url,
+                data:{
+                    parent_filter_id: parent_filter_id,
+                    attribute_id: self.attribute_id
+                },  
+                method: 'POST',
+                dataType: 'JSON',
+                success: function(data) {
+                    self.list = data.output;
+                    //self.disabled = false;
+                    self.loading = false;
+                    //$.each(data.output, function(index,item){
+                    //    self.list.push({key: item.id, label: item.name, value: item.id});
+                    //})
+                    // this.val = '';                        
+                },
+                error: function(data) {
+                    console.log(data);
+                },
+            });
+    },
         createHiddenFields(values){
             values = Array.isArray(values)?values:[values];
             $('#' + this.formFieldName).html('');
